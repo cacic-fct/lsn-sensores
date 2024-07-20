6a60a8d4fb9dd729e808bd66875d9e1f7956f9bd
Yudi <12040397+Yudi@users.noreply.github.com>
2024-07-20 15:59:46 -0300

    Limit navbar size

--- frontend/src/components/shared/Navbar.svelte
+++ frontend/src/components/shared/Navbar.svelte
@@ -11,31 +11,33 @@
 	});
 </script>
 
-<div class="glass-custom navbar sticky top-0 z-10">
-	<div class="navbar-start">
-		<div class="dropdown">
-			<div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
-				<Menu />
+<div class="glass-custom sticky top-0 z-10">
+	<div class="navbar m-auto w-full max-w-screen-2xl">
+		<div class="navbar-start">
+			<div class="dropdown">
+				<div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
+					<Menu />
+				</div>
+				<ul
+					class="menu dropdown-content menu-sm rounded-box bg-base-100 z-[1] mt-3 w-52 p-2 text-black shadow"
+				>
+					{#each list as { name, slug }}
+						<li><a class="h-12 pt-3.5" role="button" href="#{slug}">{name}</a></li>
+					{/each}
+				</ul>
 			</div>
-			<ul
-				class="menu dropdown-content menu-sm z-[1] mt-3 w-52 rounded-box bg-base-100 p-2 text-black shadow"
-			>
+			<h1 class="text-xl font-semibold">Sensores LSN</h1>
+		</div>
+		<div class="navbar-center hidden lg:flex">
+			<ul class="menu menu-horizontal px-1">
 				{#each list as { name, slug }}
-					<li><a class="h-12 pt-3.5" role="button" href="#{slug}">{name}</a></li>
+					<li><a role="button" href="#{slug}">{name}</a></li>
 				{/each}
 			</ul>
 		</div>
-		<h1 class="text-xl font-semibold">Sensores LSN</h1>
-	</div>
-	<div class="navbar-center hidden lg:flex">
-		<ul class="menu menu-horizontal px-1">
-			{#each list as { name, slug }}
-				<li><a role="button" href="#{slug}">{name}</a></li>
-			{/each}
-		</ul>
-	</div>
-	<div class="navbar-end">
-		<button class="btn"><CalendarSearch /></button>
+		<div class="navbar-end">
+			<button class="btn"><CalendarSearch /></button>
+		</div>
 	</div>
 </div>
 

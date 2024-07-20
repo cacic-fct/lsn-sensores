74e3f5012b9f80836cbb8c759d36b0f4f888265c
Yudi <12040397+Yudi@users.noreply.github.com>
2024-07-20 14:44:57 -0300

    Generate navbar dinamically

--- frontend/src/components/shared/Navbar.svelte
+++ frontend/src/components/shared/Navbar.svelte
@@ -1,5 +1,14 @@
 <script>
 	import { CalendarSearch, Menu } from 'lucide-svelte';
+
+	import { data } from '../sensors/SensorData.svelte';
+	import { createSlug } from '../sensors/SensorService.svelte';
+	const list = data.map((item) => {
+		return {
+			name: item.name,
+			slug: createSlug(item.name)
+		};
+	});
 </script>
 
 <div class="navbar bg-base-100">
@@ -9,16 +18,18 @@
 				<Menu />
 			</div>
 			<ul class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-				<li><a href="#lab-4b">Laboratório 4B</a></li>
-				<li><a href="#lsn">LSN</a></li>
+				{#each list as { name, slug }}
+					<li><a href="#{slug}">{name}</a></li>
+				{/each}
 			</ul>
 		</div>
 		<a class="text-xl">Sensores LSN</a>
 	</div>
 	<div class="navbar-center hidden lg:flex">
 		<ul class="menu menu-horizontal px-1">
-			<li><a href="#lab-4b">Laboratório 4B</a></li>
-			<li><a href="#lab-4b">LSN</a></li>
+			{#each list as { name, slug }}
+				<li><a href="#{slug}">{name}</a></li>
+			{/each}
 		</ul>
 	</div>
 	<div class="navbar-end">

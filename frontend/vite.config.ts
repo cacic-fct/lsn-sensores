b49d5ea4e68a210dff50c78823db7ed7a233e72d
Yudi <12040397+Yudi@users.noreply.github.com>
2024-07-22 17:48:20 -0300

    Fix firebase install

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,9 +4,6 @@ import { defineConfig } from 'vite';
 export default defineConfig({
 	plugins: [sveltekit()],
 	build: {
-		target: 'ES2022',
-		rollupOptions: {
-			external: ['firebase/database', 'firebase/app']
-		}
+		target: 'ES2022'
 	}
 });
